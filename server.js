70f0cd9e1f6fc886df506f551e75b9994fff69e4
Fanathing <mmybonnie@gamil.com>
2025-09-28 12:49:03 +0900

    Feat: 주말과제 기본 첫 세팅 server.js 연결 및 폴더들과 하위파일 구성

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/boards/index.html"))
+})
+
+app.listen(3000, () => {
+    console.log("시간은,,움직이기 시작한다");
+})
