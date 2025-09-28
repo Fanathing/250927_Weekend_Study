19401cd907cef3e817c4722204cbdb3d8822c2ef
Fanathing <mmybonnie@gamil.com>
2025-09-28 12:59:02 +0900

    Feat: server.js에서 넌적스, 익스프레스 언랭코디드, 라우터 설정

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("path")
+const nunjucks = require("nunjucks");
+const boardRouter = require("./boards/board.router.js");
 
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "html");
+nunjucks.configure("views", {express: app});
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/boards/index.html"))
 })
+
+app.use("boards", boardRouter);
 
 app.listen(3000, () => {
     console.log("시간은,,움직이기 시작한다");
